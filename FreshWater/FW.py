c28e0d3e2e7393e4581ae4557f66a57638fcf6e9
EirikKvamme <ekv036@uib.no>
2024-11-04 17:09:24 +0100

    Added bars for each direction and sum of each in legend. Also added 5 day low-pass filter to data

--- FreshWater/FW.py
+++ FreshWater/FW.py
@@ -1,15 +1,45 @@
 import oceanspy as ospy
 import numpy as np
 import xarray as xr
+from scipy.signal import butter, filtfilt
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyquist = 0.5 * fs
+    normal_cutoff = cutoff / nyquist
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+
+def lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = filtfilt(b, a, data)
+    return y
 
 
 def FWT(mooringDataset=xr.Dataset,ref_salinity=int):
     # Open dataset as OceanDataset
     od = ospy.OceanDataset(mooringDataset)
+
     # Subsample into monthly mean values
-    od = od.subsample.cutout(timeFreq='ME',sampMethod='mean')
+    od = od.subsample.cutout(timeFreq='D',sampMethod='mean')
+
     # Compute total transport through the mooring array
     od = od.compute.mooring_volume_transport()
+
+    # Low-pass 5 day filter
+    cutoff_frequency = 1 / (5 * 24 * 3600)  # 5 days in seconds
+    sampling_frequency = 1 / (np.diff(od.dataset['time'].values).mean() / np.timedelta64(1, 's'))  # Sampling frequency in Hz
+
+    # Apply the filter to the transport variable
+    od.dataset['transport'] = xr.apply_ufunc(
+        lowpass_filter,
+        od.dataset['transport'],
+        cutoff_frequency,
+        sampling_frequency,
+        input_core_dims=[['time']],
+        vectorize=True
+    )
 
     # Defining salinity integration depth
     transport = od.dataset['transport']
@@ -18,5 +48,16 @@ def FWT(mooringDataset=xr.Dataset,ref_salinity=int):
     
     return FW
 
-def Transport(mooringDataset=xr.Datset):
-    pass
+
+def Transport(mooringDataset=xr.Dataset):
+    # Open dataset as OceanDataset
+    od = ospy.OceanDataset(mooringDataset)
+    # Subsample into monthly mean values
+    od = od.subsample.cutout(timeFreq='D',sampMethod='mean')
+    # Compute total transport through the mooring array
+    od = od.compute.mooring_volume_transport()
+
+    # Defining salinity integration depth
+    transport = od.dataset['transport']
+    
+    return transport
